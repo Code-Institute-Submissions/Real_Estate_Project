84958416e2182a34e5847ee50a1473efc9765674
marcuscolle <marcusvcolle@gmail.com>
2020-02-18 13:29:22 +0000

    signup done

--- signup/models.py
+++ signup/models.py
@@ -3,10 +3,3 @@ from django.db import models
 
 # Create your models here.
 
-
-class Signup(models.Model):
-   
-    price = models.IntegerField()
-    
-    def __str__(self):
-        return self.name
